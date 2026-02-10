26df9ed2423a9dd5c312eac7aeac534910df43aa
ryukkaaa <reykamuhamad6@gmail.com>
2026-02-10 15:01:55 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,8 +51,15 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+
+            ->plugins([
+                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make()
+
+            ])
             ->authMiddleware([
                 Authenticate::class,
+            
+            
             ]);
     }
 }
